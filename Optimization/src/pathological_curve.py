4349869bb21d4233b989ef13196118e9f20c27c3
Nishu Choudhary <cnishu17@gmail.com>
2025-10-01 00:07:31 -0500

    Added Bivar Gaussian pathological curve

--- Optimization/src/pathological_curve.py
+++ Optimization/src/pathological_curve.py
@@ -12,8 +12,20 @@ def pathological_curve_loss(w1, w2):
     x2_loss = torch.sigmoid(w2)
     return x1_loss + x2_loss
 
+def bivar_gaussian(w1, w2, x_mean=0.0, y_mean=0.0, x_sig=1.0, y_sig=1.0):
+    norm = 1 / (2 * np.pi * x_sig * y_sig)
+    x_exp = (-1 * (w1 - x_mean)**2) / (2 * x_sig**2)
+    y_exp = (-1 * (w2 - y_mean)**2) / (2 * y_sig**2)
+    return norm * torch.exp(x_exp + y_exp)
+
+def comb_func(w1, w2):
+    z = -bivar_gaussian(w1, w2, x_mean=1.0, y_mean=-0.5, x_sig=0.2, y_sig=0.2)
+    z -= bivar_gaussian(w1, w2, x_mean=-1.0, y_mean=0.5, x_sig=0.2, y_sig=0.2)
+    z -= bivar_gaussian(w1, w2, x_mean=-0.5, y_mean=-0.8, x_sig=0.2, y_sig=0.2)
+    return z
+
 def plot_curve(curve_function, x_range=(-5,5), y_range=(-5,5),
-               title="Pathological curvature"):
+               title="BivarGaussian"):
     x = torch.arange(x_range[0], x_range[1], (x_range[1] - x_range[0]) / 100.)
     y = torch.arange(y_range[0], y_range[1], (y_range[1] - y_range[0]) / 100.)
     x, y = torch.meshgrid(x, y, indexing='xy')
@@ -38,7 +50,7 @@ def plot_curve(curve_function, x_range=(-5,5), y_range=(-5,5),
     fig.write_html(os.path.join(settings.artifacts, f"{title}.html"))
     return fig
 
-def train_curve(optimizer_func, curve_func=pathological_curve_loss, num_updates=100, init=[-1,-1]):
+def train_curve(optimizer_func, curve_func=comb_func, num_updates=100, init=[-1,-1]):
     weights = nn.Parameter(torch.FloatTensor(init), requires_grad=True)
     optimizer = optimizer_func([weights])
 
@@ -59,7 +71,7 @@ def plot_weights():
     Adam_points = train_curve(lambda params: optimizer.Adam(params, lr=1))
     all_points = np.concatenate([SGD_points, SGDMom_points, Adam_points], axis=0)
 
-    figure = plot_curve(pathological_curve_loss,
+    figure = plot_curve(comb_func,
                     x_range=(-np.absolute(all_points[:, 0]).max(), np.absolute(all_points[:, 0]).max()),
                     y_range=(all_points[:, 1].min(), all_points[:, 1].max()))
 
@@ -69,4 +81,4 @@ def plot_weights():
                                   name="SGDMom"))
     figure.add_trace(go.Scatter3d(x=Adam_points[:, 0], y=Adam_points[:, 1],z=Adam_points[:, 2],
                                 name="Adam"))
-    figure.write_html(os.path.join(settings.artifacts, f"PathologicalCurve_TrainingPlot.html"))
+    figure.write_html(os.path.join(settings.artifacts, f"BivarGaussian_TrainingPlot.html"))
